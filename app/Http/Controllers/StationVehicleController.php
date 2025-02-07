ea126cf1bfaa63c05ec3537cb68e2a1aec37b26f
Kalixto73 <Kalixto75@gmail.com>
2025-02-07 13:50:21 +0100

    feat:Add Calculation Logic To StationVehicleController

--- app/Http/Controllers/StationVehicleController.php
+++ app/Http/Controllers/StationVehicleController.php
@@ -10,6 +10,24 @@ class StationVehicleController extends Controller
     public function index()
     {
         $stationVehicles = StationVehicle::all();
+        $stationVehicles->each(function ($stationVehicle) {
+            $stationVehicle->totalPrice = $this->calculateTollPrice($stationVehicle->id);
+        });
         return view('stationVehicles', compact('stationVehicles'));
+    }
+    private function calculateTollPrice($stationVehicleId)
+    {
+        $stationVehicle = StationVehicle::with(['vehicle.vehicleType'])->findOrFail($stationVehicleId);
+
+        $vehicle = $stationVehicle->vehicle;
+        $vehicleType = $vehicle->vehicleType;
+
+        $baseFee = $vehicleType->base_fee;
+        $additionalFee = $vehicleType->additional_base_fee * ($vehicle->axles);
+
+        $totalPriceOfFee = $baseFee + $additionalFee;
+        $totalPrice = $totalPriceOfFee * $stationVehicle->pass_count;
+        
+        return $totalPrice;
     }
 }
