03b4081bf751c17e55605d85c8f11212acb4de2e
Kalixto73 <Kalixto75@gmail.com>
2025-02-07 10:49:17 +0100

    feat:Create StationVehicle Migration

---
+++ database/migrations/2025_02_07_094559_create_station_vehicle_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('station_vehicle', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('stations_id')->constrained('stations')->onDelete('cascade');
+            $table->foreignId('vehicles_id')->constrained('vehicles')->onDselete('cascade');
+            $table->bigInteger('total_collected');
+            $table->bigInteger('pass_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('station_vehicle');
+    }
+};
