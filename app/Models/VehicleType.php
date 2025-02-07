0d725d991a9df9659ac795edecf1a613af038c40
Kalixto73 <Kalixto75@gmail.com>
2025-02-07 11:31:20 +0100

    feat:Create VehicleType Model

---
+++ app/Models/VehicleType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class VehicleType extends Model
+{
+    use HasFactory;
+    protected $table = 'vehicle_types';
+    protected $fillable = [
+        'vehicle_type',
+        'base_fee',
+        'aditional_base_fee',
+    ];
+    public function vehicles()
+    {
+        return $this->hasMany(Vehicle::class);
+    }
+}
